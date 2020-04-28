dded5b5df56fc052afb26fa12247074360cf9d3f
Adam Bottchen <adam.bottchen@puppet.com>
2020-04-27 18:25:55 -0700

    redirect output to avoid bolt plan error

--- tasks/extract_supporting_files.sh
+++ tasks/extract_supporting_files.sh
@@ -3,4 +3,4 @@ tar -xvf /tmp/etc.tar.gz -C /
 rm /tmp/etc.tar.gz
 docker exec -it gitlab sed -ie "s/ask_to_continue//g" /opt/gitlab/embedded/service/gitlab-rails/lib/tasks/gitlab/backup.rake
 docker exec -it gitlab sed -ie "s/ask_to_continue//g" /opt/gitlab/embedded/service/gitlab-rails/lib/tasks/gitlab/shell.rake
-docker exec -it gitlab gitlab-backup restore
+docker exec -it gitlab gitlab-backup restore > /tmp/gitlab_restore.log

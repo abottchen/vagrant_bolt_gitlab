69df0b1148b33aee69f3b8baba53f3af5b4f353f
Adam Bottchen <adam.bottchen@puppet.com>
2020-04-27 18:49:07 -0700

    Adding debug lines and removing tty flag from docker

--- tasks/extract_supporting_files.sh
+++ tasks/extract_supporting_files.sh
@@ -1,6 +1,11 @@
 #!/bin/bash
+echo "Extracting configuration files"
 tar -xvf /tmp/etc.tar.gz -C /
+echo "Removing first prompt from restore script"
+docker exec -i gitlab sed -ie "s/ask_to_continue//g" /opt/gitlab/embedded/service/gitlab-rails/lib/tasks/gitlab/backup.rake
+echo "Removing second prompt from restore script"
+docker exec -i gitlab sed -ie "s/ask_to_continue//g" /opt/gitlab/embedded/service/gitlab-rails/lib/tasks/gitlab/shell.rake
+echo "Running gitlab restore"
+docker exec -i gitlab gitlab-backup restore > /tmp/gitlab_restore.log
+echo "Cleaning up configuration data"
 rm /tmp/etc.tar.gz
-docker exec -it gitlab sed -ie "s/ask_to_continue//g" /opt/gitlab/embedded/service/gitlab-rails/lib/tasks/gitlab/backup.rake
-docker exec -it gitlab sed -ie "s/ask_to_continue//g" /opt/gitlab/embedded/service/gitlab-rails/lib/tasks/gitlab/shell.rake
-docker exec -it gitlab gitlab-backup restore > /tmp/gitlab_restore.log

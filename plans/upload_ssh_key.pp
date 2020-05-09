a246e62149a7c88df926bc347cbbfad7bbc6b684
Adam Bottchen <adam.bottchen@puppet.com>
2020-05-08 17:30:52 -0700

    Whitespace fix

--- plans/upload_ssh_key.pp
+++ plans/upload_ssh_key.pp
@@ -7,9 +7,9 @@ plan vagrant_bolt_gitlab::upload_ssh_key(
     file {'/etc/puppetlabs/puppetserver/ssh.key':
       ensure => file,
       source => 'puppet:///modules/vagrant_bolt_gitlab/ssh.key',
-      owner => 'pe-puppet',
-      group => 'pe-puppet',
-      mode => '0640',
+      owner  => 'pe-puppet',
+      group  => 'pe-puppet',
+      mode   => '0640',
     }
   }
 }
